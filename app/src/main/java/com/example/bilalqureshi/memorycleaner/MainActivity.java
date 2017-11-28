879b015d60a5c162ca5683e12e5b8fb7b6721fbc
bilalqureshi123 <33433166+bilalqureshi123@users.noreply.github.com>
2017-11-28 11:40:49 -0800

    Update MainActivity.java

--- app/src/main/java/com/example/bilalqureshi/memorycleaner/MainActivity.java
+++ app/src/main/java/com/example/bilalqureshi/memorycleaner/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.bilalqureshi.memorycleaner;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
@@ -12,6 +13,7 @@ import android.os.Build;
 import android.os.Environment;
 import android.os.StatFs;
 import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -28,6 +30,28 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "Applications List   ";
     private Context context;
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            android.Manifest.permission.READ_EXTERNAL_STORAGE
+
+
+    };
+
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+
+            );
+        }
+    }
 
     //space
 
@@ -141,11 +165,16 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        verifyStoragePermissions(this);
+
         TextView textView1 = (TextView) findViewById(R.id.memoryview);
         TextView textView2 = (TextView) findViewById(R.id.memoryview2);
 
         long a = TotalMemory();
         long b = FreeMemory();
+
+
 
         String bj = bytesToHuman(a);
         String cd = bytesToHuman(b);
